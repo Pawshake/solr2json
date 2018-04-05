8817f266b46164bcd3e2ef70b73cdc5b6dae755d
Steven Teerlinck <steven@teerlinck.net>
2018-04-05 13:07:39 +0200

    refactored Transformer to loose some weight

--- src/Transformer/Transformer.php
+++ src/Transformer/Transformer.php
@@ -6,7 +6,6 @@ class Transformer
 {
     public function transform(array $data)
     {
-
         $geocode = $data['locm_field_host_geocode'];
         $location = explode(',', $geocode[0]);
 
@@ -17,91 +16,28 @@ class Transformer
 
         $serviceRates = [];
 
-        if (isset($data['bm_field_host_pet_boarding'], $data['fts_field_host_rate_per_night']) && $data['bm_field_host_pet_boarding'][0] === 1) {
-            $result = $data['fts_field_host_rate_per_night'];
-            $serviceRates['petBoarding'] = (float) reset($result);
+        if (isset($data['bm_field_host_pet_boarding'], $data['fts_field_host_rate_per_night'])
+            && $data['bm_field_host_pet_boarding'][0] === 1) {
+            $serviceRates['petBoarding'] = (float) reset($data['fts_field_host_rate_per_night']);
         }
-        if (isset($data['bm_field_host_walking'], $data['fts_field_dog_walking_rate']) && $data['bm_field_host_walking'][0] === 1) {
-            $result = $data['fts_field_dog_walking_rate'];
-            $serviceRates['dogWalking'] = (float) reset($result);
+        if (isset($data['bm_field_host_walking'], $data['fts_field_dog_walking_rate'])
+            && $data['bm_field_host_walking'][0] === 1) {
+            $serviceRates['dogWalking'] = (float) reset($data['fts_field_dog_walking_rate']);
         }
 
-        if (isset($data['bm_field_host_daycare_in_my_home'], $data['fts_field_host_doggy_day_care_ra']) && $data['bm_field_host_daycare_in_my_home'][0] === 1) {
-            $result = $data['fts_field_host_doggy_day_care_ra'];
-            $serviceRates['doggyDayCare'] = (float) reset($result);
+        if (isset($data['bm_field_host_daycare_in_my_home'], $data['fts_field_host_doggy_day_care_ra'])
+            && $data['bm_field_host_daycare_in_my_home'][0] === 1) {
+            $serviceRates['doggyDayCare'] = (float) reset($data['fts_field_host_doggy_day_care_ra']);
         }
         if (isset($data['bs_home_visits'], $data['bs_home_visits'])) {
             if (isset($data['fts_field_host_rate_one_home_vis'])) {
-                $one_visit_result = $data['fts_field_host_rate_one_home_vis'];
-                $serviceRates['homeVisits'] = (float) ($one_visit_result);
+                $serviceRates['homeVisits'] = (float) ($data['fts_field_host_rate_one_home_vis']);
             }
 
         }
-        if (isset($data['bm_field_host_sleepover'], $data['fts_field_host_rate_sleepover']) && $data['bm_field_host_sleepover'][0] === 1) {
-            $result = $data['fts_field_host_rate_sleepover'];
-            $serviceRates['sleepover'] = (float) reset($result);
-        }
-
-        $inquiry_info = unserialize($data['zs_inquiry_info']);
-        $integers = [
-            'unique_users_inquiry',
-            'unique_users_pre_approved',
-            'unique_users_pre_approved_and_made_a_booking',
-            'unique_users_declined',
-            'unique_users_direct_booking',
-            'unique_users_booking_all',
-            'unique_users_pre_approved_90d',
-            'unique_users_pre_approved_and_made_a_booking_90d',
-            'total_inquiries',
-            'total_inquiries_preapproved',
-            'total_inquiries_declined',
-            'total_inquiries_booking',
-            'unique_users_that_made_a_booking',
-        ];
-        foreach ($integers as $integer) {
-            if (isset($inquiry_info[$integer])) {
-                $inquiry_info[$integer] = (int) $inquiry_info[$integer];
-            }
-        }
-        $pm_info = unserialize($data['zs_pm_info']);
-        $integers = [
-            'threads_all',
-            'messages_all',
-            'answered_all',
-            'threads_all_rate',
-            'answered_all_rate',
-            'threads_all_rate_45_days',
-            'answered_all_rate_45_days',
-            'threads_all_rate_7_days',
-            'answered_all_rate_7_days',
-            'answered_all_rate_7_days',
-            'response_rate_7_days',
-            'unread',
-            'unanswered_all',
-            'avg_response_time_int_all',
-            'unanswered_45',
-            'avg_response_time_int_45',
-            'unanswered_14',
-            'avg_response_time_int_14',
-            'unanswered_7',
-            'response_rate_7_days',
-            'avg_response_time_int_7',
-            'unique_users_life_time',
-            'unique_users_90_days',
-        ];
-        foreach ($integers as $integer) {
-            if (isset($pm_info[$integer])) {
-                $pm_info[$integer] = (int) $pm_info[$integer];
-            }
-        }
-        $floats = [
-            'response_rate',
-            'response_rate_45_days',
-        ];
-        foreach ($floats as $float) {
-            if (isset($pm_info[$float])) {
-                $pm_info[$float] = (float) $pm_info[$float];
-            }
+        if (isset($data['bm_field_host_sleepover'], $data['fts_field_host_rate_sleepover'])
+            && $data['bm_field_host_sleepover'][0] === 1) {
+            $serviceRates['sleepover'] = (float) reset($data['fts_field_host_rate_sleepover']);
         }
 
         return [
